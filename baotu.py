3c7d675d9042afef240432d372868bec2428b838
Joynice <woshiliran@foxmai.com>
2019-04-01 13:35:39 +0800

    提交代码

---
+++ baotu.py
@@ -0,0 +1,82 @@
+# -*- coding: UTF-8 -*-
+__author__ = 'Joynice'
+import requests
+import os
+from bs4 import BeautifulSoup
+import queue
+import threading
+class Baotu(object):
+    '''
+    负责爬虫存储
+    TODO:
+        多线程
+
+    '''
+    def __init__(self, url='https://ibaotu.com/shipin/', thread=1, max_page=250, useragent=None):
+        '''
+
+        :param url:
+        :param thread: 线程数
+        :param max_page: 爬取页数
+        :param header: 请求useragent
+        '''
+        self.url = url
+        self.thread = thread
+        self.page = max_page
+        self.useragent = useragent
+        self.header = self._get_header()
+        self.que = queue.Queue()
+
+    def _get_header(self):
+        if not isinstance(self.useragent, str):
+            self.useragent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
+            return {'User-Agent': self.useragent}
+
+
+    def request(self):
+        for i in range(1, self.page+1):
+            try:
+                print(self.url)
+                req = requests.get(self.url + '6-0-0-0-0-{}.html'.format(i), headers=self.header, timeout=10, verify=True)
+                print('正在爬取第%d页的数据' %i)
+                if req.status_code == 200:
+                    bs = BeautifulSoup(req.text)
+                    for _, n in zip(bs.find_all('video', src=True), bs.find_all('img', {'class': 'scrollLoading'})):
+                        self.que.put({'url': 'http:'+_['src'], 'name':n['alt']})
+            except Exception as e:
+                print(e)
+                pass
+        print('共有{}条视频需要下载！'.format(self.que.qsize()))
+
+
+    def download(self, path=os.getcwd()):
+        while not self.que.empty():
+            data = self.que.get()
+            try:
+                req = requests.get(url=data['url'],headers=self.header, verify=True)
+
+                if req.status_code == 200:
+                    print(data['name'])
+                    if os.path.exists(path):
+                        with open(os.path.join(path, data['name']), 'wb') as f:
+                            f.write(req.content)
+                    else:
+                        raise Exception('文件夹不存在')
+            except Exception as e:
+                print(e)
+                continue
+
+    def run(self):
+        t1 = threading.Thread(target=self.request)
+        thread_list = []
+        for i in range(self.thread+1):
+            t = threading.Thread(target=self.download, args=('D:\\video',))
+            thread_list.append(t)
+        t1.start()
+        for t in thread_list:
+            t.start()
+
+
+if __name__ == '__main__':
+    a = Baotu(max_page=200, thread=10).run()
+# D:\video
